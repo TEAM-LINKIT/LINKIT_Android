b027dc659bb94fed4a8896329bf4534d4ad08a95
heewon <ymcho24@naver.com>
2021-04-15 17:29:29 +0900

    [feat] 게시물 상세보기 뷰 구현

---
+++ app/src/main/java/com/example/linkit_android/upload/PostingActivity.kt
@@ -0,0 +1,22 @@
+package com.example.linkit_android.upload
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.linkit_android.databinding.ActivityPostingBinding
+
+class PostingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPostingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setViewBinding()
+    }
+
+    private fun setViewBinding() {
+        binding = ActivityPostingBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
