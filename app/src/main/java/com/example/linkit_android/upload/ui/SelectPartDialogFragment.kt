1995b52414387601811b0f9f8d3890e933bb7756
heewon <ymcho24@naver.com>
2021-04-27 00:52:56 +0900

    [feat] 모집 파트/모집 인원 설정 다이얼로그 레이아웃 구현

---
+++ app/src/main/java/com/example/linkit_android/upload/ui/SelectPartDialogFragment.kt
@@ -0,0 +1,41 @@
+package com.example.linkit_android.upload.ui
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.example.linkit_android.databinding.DialogSelectPartBinding
+import com.example.linkit_android.util.setDialogSize
+
+class SelectPartDialogFragment : DialogFragment() {
+
+    private var _binding: DialogSelectPartBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        _binding = DialogSelectPartBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setDialogSize(dialog, activity)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
