10283e65006a4ae953cfff99389c21e79c572d15
heewon <ymcho24@naver.com>
2021-05-16 03:41:29 +0900

    [feat] 커뮤니티 뷰 선택 파트 색상 변경 및 데이터 업데이트 기능 추가

--- app/src/main/java/com/example/linkit_android/community/ui/CommunityFragment.kt
+++ app/src/main/java/com/example/linkit_android/community/ui/CommunityFragment.kt
@@ -1,7 +1,13 @@
 package com.example.linkit_android.community.ui
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.TextUtils
+import android.text.style.ForegroundColorSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -18,7 +24,6 @@ import com.example.linkit_android.model.PostingModel
 import com.example.linkit_android.upload.ui.UploadActivity
 import com.example.linkit_android.util.SharedPreferenceController
 import com.google.firebase.database.*
-import java.lang.StringBuilder
 
 class CommunityFragment : Fragment() {
 
@@ -32,6 +37,7 @@ class CommunityFragment : Fragment() {
 
     private lateinit var userName: String
     private lateinit var profileImg: String
+    private var userPart = -1
 
     private val firebaseDatabase: FirebaseDatabase = FirebaseDatabase.getInstance()
     private val databaseReference: DatabaseReference = firebaseDatabase.reference
@@ -51,9 +57,9 @@ class CommunityFragment : Fragment() {
 
         initProfile()
 
-        initRecyclerView()
-
         initPartSpinner()
+
+        initRecyclerView()
 
         goToUploadActivity()
     }
@@ -62,6 +68,7 @@ class CommunityFragment : Fragment() {
         SharedPreferenceController.apply {
             userName = getUserName(context!!).toString()
             profileImg = getProfileImg(context!!).toString()
+            userPart = getUserPart(context!!)
         }
     }
 
@@ -69,6 +76,32 @@ class CommunityFragment : Fragment() {
         binding.apply {
             tvUserName.text = userName
             Glide.with(context!!).load(profileImg).into(imgProfile)
+        }
+    }
+
+    private fun initPartSpinner() {
+        val item = resources.getStringArray(R.array.part_array)
+        val partAdapter = ArrayAdapter(context!!, R.layout.item_part_spinner, item)
+        partAdapter.setDropDownViewResource(R.layout.item_part_spinner_content)
+
+        binding.spinnerPart.apply {
+            adapter = partAdapter
+            setSelection(userPart)
+        }
+        setSelectedListenerOnSpinner()
+    }
+
+    private fun setSelectedListenerOnSpinner() {
+        binding.spinnerPart.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                when (position) {
+                    0 -> getPostingData(position)
+                    1 -> getPostingData(position)
+                    2 -> getPostingData(position)
+                    3 -> getPostingData(position)
+                }
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
         }
     }
 
@@ -80,27 +113,6 @@ class CommunityFragment : Fragment() {
         }
     }
 
-    private fun initPartSpinner() {
-        val item = resources.getStringArray(R.array.part_array)
-        val partAdapter = ArrayAdapter(context!!, R.layout.item_part_spinner, item)
-        partAdapter.setDropDownViewResource(R.layout.item_part_spinner_content)
-
-        binding.spinnerPart.apply {
-            adapter = partAdapter
-            onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                    when (position) {
-                        0 -> getPostingData(position)
-                        1 -> getPostingData(position)
-                        2 -> getPostingData(position)
-                        3 -> getPostingData(position)
-                    }
-                }
-                override fun onNothingSelected(p0: AdapterView<*>?) {}
-            }
-        }
-    }
-
     private fun getPostingData(position: Int) {
         selectPart = position
         databaseReference.child("community").addListenerForSingleValueEvent(object: ValueEventListener {
@@ -108,7 +120,7 @@ class CommunityFragment : Fragment() {
                 postingList.clear()
                 for (data in snapshot.children) {
                     val postingData = data.getValue(PostingModel::class.java)
-                    if (postingData?.recruitNum!![position] != 0)
+                    if (postingData?.recruitNum!![selectPart] != 0)
                         postingList.add(postingData)
                 }
                 bindData()
@@ -125,17 +137,32 @@ class CommunityFragment : Fragment() {
         communityAdapter.notifyDataSetChanged()
     }
 
-    private fun setPartListData(partList: MutableList<Int>) : String {
-        val partString = StringBuilder()
-        if (partList[0] != 0)
-            partString.append("기획")
-        if (partList[1] != 0)
-            partString.append("디자인")
-        if (partList[2] != 0)
-            partString.append("프론드엔드")
-        if (partList[3] != 0)
-            partString.append("백엔드")
-        return partString.toString()
+    private fun setPartListData(partList: MutableList<Int>) : CharSequence {
+        var partString: CharSequence = ""
+        for (i in 0 until partList.size) {
+            if (partList[i] != 0) {
+                when (i) {
+                    0 -> partString = TextUtils.concat(partString, setTextColor("기획", 0))
+                    1 -> partString = TextUtils.concat(partString, setTextColor("디자인", 1))
+                    2 -> partString = TextUtils.concat(partString, setTextColor("프론트엔드", 2))
+                    3 -> partString = TextUtils.concat(partString, setTextColor("백엔드", 3))
+                }
+                partString = TextUtils.concat(partString, " · ")
+            }
+        }
+        if (partString.isNotEmpty())
+            partString = partString.dropLast(3)
+        return partString
+    }
+
+    @SuppressLint("ResourceAsColor")
+    private fun setTextColor(partString: String, index: Int) : SpannableStringBuilder {
+        val spannableString = SpannableStringBuilder(partString)
+        if (index == selectPart) {
+            spannableString.setSpan(ForegroundColorSpan(Color.parseColor("#F56457")), 0, partString.length,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        }
+        return spannableString
     }
 
     private fun goToUploadActivity() {
@@ -143,6 +170,16 @@ class CommunityFragment : Fragment() {
             val intent = Intent(context!!, UploadActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        updateData()
+    }
+
+    private fun updateData() {
+        getPostingData(binding.spinnerPart.selectedItemPosition)
     }
 
     override fun onDestroyView() {
