a5a331110c55eacc31e702808092746b95d4d9fc
heewon <ymcho24@naver.com>
2021-05-17 23:13:11 +0900

    [feat] 포트폴리오 협업 툴 다이얼로그 기능 구현 및 서버 통신

--- app/src/main/java/com/example/linkit_android/portfolio/adapter/StrokeRedTagAdapter.kt
+++ app/src/main/java/com/example/linkit_android/portfolio/adapter/StrokeRedTagAdapter.kt
@@ -5,10 +5,13 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.linkit_android.R
+import com.example.linkit_android.util.ItemClickListener
+import kotlinx.android.synthetic.main.item_tag_stroke_red.view.*
 
 class StrokeRedTagAdapter(private val context: Context) : RecyclerView.Adapter<StrokeRedTagViewHolder>() {
 
     var data = mutableListOf<String>()
+    private lateinit var itemClickListener: ItemClickListener
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StrokeRedTagViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.item_tag_stroke_red, parent, false)
@@ -16,8 +19,17 @@ class StrokeRedTagAdapter(private val context: Context) : RecyclerView.Adapter<S
     }
 
     override fun onBindViewHolder(holder: StrokeRedTagViewHolder, position: Int) {
-        holder.onBind(data[position])
+        holder.apply {
+            onBind(data[position])
+            itemView.item_tag_stroke.setOnClickListener {
+                itemClickListener.onClickItem(it, position)
+            }
+        }
     }
 
     override fun getItemCount(): Int = data.size
+
+    fun setItemClickListener(itemClickListener: ItemClickListener) {
+        this.itemClickListener = itemClickListener
+    }
 }
--- app/src/main/java/com/example/linkit_android/portfolio/ui/ToolDialogFragment.kt
+++ app/src/main/java/com/example/linkit_android/portfolio/ui/ToolDialogFragment.kt
@@ -6,15 +6,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.example.linkit_android.databinding.DialogToolBinding
 import com.example.linkit_android.portfolio.adapter.FillRedTagAdapter
 import com.example.linkit_android.portfolio.adapter.StrokeRedTagAdapter
+import com.example.linkit_android.util.ItemClickListener
+import com.example.linkit_android.util.SharedPreferenceController
+import com.example.linkit_android.util.hideKeyboard
 import com.example.linkit_android.util.setDialogSize
 import com.google.android.flexbox.FlexDirection
 import com.google.android.flexbox.FlexWrap
 import com.google.android.flexbox.FlexboxLayoutManager
 import com.google.android.flexbox.JustifyContent
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class ToolDialogFragment : DialogFragment() {
 
@@ -23,6 +29,13 @@ class ToolDialogFragment : DialogFragment() {
 
     private lateinit var inputToolAdapter: FillRedTagAdapter
     private lateinit var exampleToolAdapter: StrokeRedTagAdapter
+
+    private lateinit var inputToolList: MutableList<String>
+
+    private lateinit var uid: String
+
+    private val firebaseDatabase : FirebaseDatabase = FirebaseDatabase.getInstance()
+    private val databaseReference : DatabaseReference = firebaseDatabase.reference
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
@@ -33,9 +46,44 @@ class ToolDialogFragment : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setPref()
+
+        getIntentValue()
+
+        setRecyclerViewVisibility(inputToolList)
+
         initInputToolRecyclerView()
 
         initExampleToolRecyclerView()
+
+        initAddBtn()
+
+        initOkBtn()
+
+        initCancel()
+    }
+
+    /*
+        PortfolioFragment
+        1. firebase에서 data 불러오기 -> 없으면 내용이 없습니다 출력 (onResume)
+        2. 불러온 data intent에 담아 ToolDialogFrag로 보내기
+        3. ToolDialogFrag에서 intent 받기
+    */
+
+    private fun setPref() {
+        uid = SharedPreferenceController.getUid(context!!).toString()
+    }
+
+    private fun getIntentValue() {
+        // Todo: intent로 받아온 배열 값이 empty가 아니면 inputToolList에 넣어주고, empty일 경우 inputToolList를 mutableListOf()로 초기화
+        inputToolList = mutableListOf()
+    }
+
+    private fun setRecyclerViewVisibility(data: MutableList<String>) {
+        if (data.isEmpty())
+            binding.constraintlayoutInputTool.visibility = View.GONE
+        else
+            binding.constraintlayoutInputTool.visibility = View.VISIBLE
     }
 
     private fun initInputToolRecyclerView() {
@@ -51,9 +99,18 @@ class ToolDialogFragment : DialogFragment() {
         }
 
         inputToolAdapter.apply {
-            data = mutableListOf("GitHub", "Slack", "Figma")
+            data = inputToolList
+            initInputItemClickListener()
             notifyDataSetChanged()
         }
+    }
+
+    private fun initInputItemClickListener() {
+        inputToolAdapter.setItemClickListener(object: ItemClickListener {
+            override fun onClickItem(view: View, position: Int) {
+                setRecyclerViewVisibility(inputToolList)
+            }
+        })
     }
 
     private fun initExampleToolRecyclerView() {
@@ -70,7 +127,85 @@ class ToolDialogFragment : DialogFragment() {
 
         exampleToolAdapter.apply {
             data = mutableListOf("GitHub", "Notion", "Trello", "Slack", "Jira", "Zeplin", "Figma")
+            initExampleItemClickListener()
             notifyDataSetChanged()
+        }
+    }
+
+    private fun initExampleItemClickListener() {
+        exampleToolAdapter.setItemClickListener(object: ItemClickListener {
+            override fun onClickItem(view: View, position: Int) {
+                addExampleItemToRecyclerView(position)
+            }
+        })
+    }
+
+    private fun addExampleItemToRecyclerView(position: Int) {
+        if (inputToolAdapter.data.isEmpty())
+            addExampleItemToAdapter(position)
+        else {
+            if (!checkDuplicate(exampleToolAdapter.data[position]))
+                addExampleItemToAdapter(position)
+        }
+    }
+
+    private fun checkDuplicate(checkData: String) : Boolean {
+        var flag = false
+        for (item in inputToolAdapter.data) {
+            if (item == checkData) {
+                flag = true
+                break
+            }
+        }
+        return flag
+    }
+
+    private fun addExampleItemToAdapter(position: Int) {
+        inputToolAdapter.apply {
+            data.add(exampleToolAdapter.data[position])
+            setRecyclerViewVisibility(data)
+            notifyDataSetChanged()
+        }
+    }
+
+    private fun initAddBtn() {
+        binding.btnAddTag.setOnClickListener {
+            val inputData = binding.etContent.text.toString()
+            if (inputData != "") {
+                if (inputToolAdapter.data.isEmpty())
+                    addInputDataToRecyclerView(inputData)
+                else {
+                    if (!checkDuplicate(inputData))
+                        addInputDataToRecyclerView(inputData)
+                }
+            } else {
+                Toast.makeText(context!!, "내용을 입력해주세요", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun addInputDataToRecyclerView(inputData: String) {
+        binding.etContent.setText("")
+        inputToolAdapter.apply {
+            data.add(inputData)
+            setRecyclerViewVisibility(data)
+            notifyDataSetChanged()
+        }
+    }
+
+    private fun initOkBtn() {
+        binding.buttonOk.setOnClickListener {
+            databaseReference.child("users").child(uid).child("portfolio")
+                    .child("tool").setValue(inputToolAdapter.data)
+            hideKeyboard(context!!, binding.etContent)
+            this.dismiss()
+        }
+    }
+
+    private fun initCancel() {
+        binding.buttonCancel.setOnClickListener {
+            hideKeyboard(context!!, binding.etContent)
+            this.dismiss()
         }
     }
 
