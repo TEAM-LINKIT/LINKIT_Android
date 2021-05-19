ce85294b23cce30a4923b6ba3f9be4faff2c4d60
heewon <ymcho24@naver.com>
2021-05-19 17:20:13 +0900

    [feat] 포트폴리오 관심 분야 다이얼로그 기능 구현 및 서버 통신

--- app/src/main/java/com/example/linkit_android/portfolio/ui/FieldDialogFragment.kt
+++ app/src/main/java/com/example/linkit_android/portfolio/ui/FieldDialogFragment.kt
@@ -6,15 +6,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.example.linkit_android.databinding.DialogFieldBinding
 import com.example.linkit_android.portfolio.adapter.FillRedTagAdapter
 import com.example.linkit_android.portfolio.adapter.StrokeRedTagAdapter
+import com.example.linkit_android.util.ItemClickListener
+import com.example.linkit_android.util.SharedPreferenceController
+import com.example.linkit_android.util.hideKeyboard
 import com.example.linkit_android.util.setDialogSize
 import com.google.android.flexbox.FlexDirection
 import com.google.android.flexbox.FlexWrap
 import com.google.android.flexbox.FlexboxLayoutManager
 import com.google.android.flexbox.JustifyContent
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class FieldDialogFragment : DialogFragment() {
 
@@ -23,6 +29,13 @@ class FieldDialogFragment : DialogFragment() {
 
     private lateinit var inputFieldAdapter: FillRedTagAdapter
     private lateinit var exampleFieldAdapter: StrokeRedTagAdapter
+
+    private lateinit var inputFieldList: MutableList<String>
+
+    private lateinit var uid: String
+
+    private val firebaseDatabase : FirebaseDatabase = FirebaseDatabase.getInstance()
+    private val databaseReference : DatabaseReference = firebaseDatabase.reference
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
@@ -33,9 +46,36 @@ class FieldDialogFragment : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setPref()
+
+        getIntentValue()
+
+        setRecyclerViewVisibility(inputFieldList)
+
         initInputFieldRecyclerView()
 
         initExampleFieldRecyclerView()
+
+        initAddBtn()
+
+        initOkBtn()
+
+        initCancelBtn()
+    }
+
+    private fun setPref() {
+        uid = SharedPreferenceController.getUid(context!!).toString()
+    }
+
+    private fun getIntentValue() {
+        inputFieldList = mutableListOf()
+    }
+
+    private fun setRecyclerViewVisibility(data: MutableList<String>) {
+        if (data.isEmpty())
+            binding.constraintlayoutInputField.visibility = View.GONE
+        else
+            binding.constraintlayoutInputField.visibility = View.VISIBLE
     }
 
     private fun initInputFieldRecyclerView() {
@@ -51,9 +91,18 @@ class FieldDialogFragment : DialogFragment() {
         }
 
         inputFieldAdapter.apply {
-            data = mutableListOf("aws", "Java script")
+            data = inputFieldList
+            initInputItemClickListener()
             notifyDataSetChanged()
         }
+    }
+
+    private fun initInputItemClickListener() {
+        inputFieldAdapter.setItemClickListener(object: ItemClickListener {
+            override fun onClickItem(view: View, position: Int) {
+                setRecyclerViewVisibility(inputFieldList)
+            }
+        })
     }
 
     private fun initExampleFieldRecyclerView() {
@@ -71,7 +120,84 @@ class FieldDialogFragment : DialogFragment() {
         exampleFieldAdapter.apply {
             data = mutableListOf("서비스기획", "컨텐츠기획", "Web Design", "App Design",
                     "Android", "iOS", "HTML", "MySQL", "Spring", "Node.js")
+            initExampleItemClickListener()
             notifyDataSetChanged()
+        }
+    }
+
+    private fun initExampleItemClickListener() {
+        exampleFieldAdapter.setItemClickListener(object: ItemClickListener {
+            override fun onClickItem(view: View, position: Int) {
+                addExampleItemToRecyclerView(position)
+            }
+        })
+    }
+
+    private fun addExampleItemToRecyclerView(position: Int) {
+        if (inputFieldAdapter.data.isEmpty())
+            addExampleItemToAdapter(position)
+        else
+            if (!checkDuplicate(exampleFieldAdapter.data[position]))
+                addExampleItemToAdapter(position)
+    }
+
+    private fun checkDuplicate(checkData: String) : Boolean {
+        var flag = false
+        for (item in inputFieldAdapter.data) {
+            if (item == checkData) {
+                flag = true
+                break
+            }
+        }
+        return flag
+    }
+
+    private fun addExampleItemToAdapter(position: Int) {
+        inputFieldAdapter.apply {
+            data.add(exampleFieldAdapter.data[position])
+            setRecyclerViewVisibility(data)
+            notifyDataSetChanged()
+        }
+    }
+
+    private fun initAddBtn() {
+        binding.btnAddTag.setOnClickListener {
+            val inputData = binding.etContent.text.toString()
+            if (inputData != "") {
+                if (inputFieldAdapter.data.isEmpty())
+                    addInputDataToRecyclerView(inputData)
+                else {
+                    if (!checkDuplicate(inputData))
+                        addInputDataToRecyclerView(inputData)
+                }
+            } else {
+                Toast.makeText(context!!, "내용을 입력해주세요", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun addInputDataToRecyclerView(inputData: String) {
+        binding.etContent.setText("")
+        inputFieldAdapter.apply {
+            data.add(inputData)
+            setRecyclerViewVisibility(data)
+            notifyDataSetChanged()
+        }
+    }
+
+    private fun initOkBtn() {
+        binding.buttonOk.setOnClickListener {
+            databaseReference.child("users").child(uid).child("portfolio")
+                    .child("field").setValue(inputFieldAdapter.data)
+            hideKeyboard(context!!, binding.etContent)
+            this.dismiss()
+        }
+    }
+
+    private fun initCancelBtn() {
+        binding.buttonCancel.setOnClickListener {
+            hideKeyboard(context!!, binding.etContent)
+            this.dismiss()
         }
     }
 
