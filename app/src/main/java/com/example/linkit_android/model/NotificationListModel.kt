9e9724785b08a19d46d28717b1f586b1ec450638
heewon <ymcho24@naver.com>
2021-05-25 13:20:52 +0900

    [fix] NotificationListModel.kt 구조 수정

--- app/src/main/java/com/example/linkit_android/model/NotificationListModel.kt
+++ app/src/main/java/com/example/linkit_android/model/NotificationListModel.kt
@@ -2,6 +2,6 @@ package com.example.linkit_android.model
 
 data class NotificationListModel (
         var uid: String? = null,
-        var postingId: String? = null,
+        var postingTitle: String? = null,
         var read: Boolean? = null
         )
