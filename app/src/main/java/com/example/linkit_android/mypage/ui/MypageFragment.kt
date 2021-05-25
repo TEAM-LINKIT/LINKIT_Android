b8c6991b8477be13cc491b780372a670d8c39476
YOUNGMIN KIM <72733983+zeromin0o0@users.noreply.github.com>
2021-05-25 14:06:47 +0900

    [feat] 마이페이지 뷰 기능 구현 및 서버 통신

--- app/src/main/java/com/example/linkit_android/mypage/ui/MypageFragment.kt
+++ app/src/main/java/com/example/linkit_android/mypage/ui/MypageFragment.kt
@@ -1,11 +1,16 @@
 package com.example.linkit_android.mypage.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.example.linkit_android.chatting.ui.ChatRoomActivity
 import com.example.linkit_android.databinding.FragmentMypageBinding
+import com.example.linkit_android.util.SharedPreferenceController
+import com.example.linkit_android.util.getPartString
 
 class MypageFragment : Fragment() {
 
@@ -22,6 +27,37 @@ class MypageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initMypage()
+
+        initWriteListBtn()
+
+        initRecommendListBtn()
+    }
+
+    private fun initMypage() {
+        val name = SharedPreferenceController.getUserName(context!!).toString()
+        val part = SharedPreferenceController.getUserPart(context!!)
+        val profileImg = SharedPreferenceController.getProfileImg(context!!).toString()
+
+        binding.tvUserName.text = name
+        binding.tvPart.text = getPartString(part)
+
+        Glide.with(context!!).load(profileImg).into(binding.imgProfile)
+    }
+
+    private fun initWriteListBtn() {
+        binding.btnWriteList.setOnClickListener {
+            val intent = Intent(context, PostingListActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun initRecommendListBtn() {
+        binding.btnRecommendList.setOnClickListener {
+            val intent = Intent(context, RecommendListActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onDestroyView() {
