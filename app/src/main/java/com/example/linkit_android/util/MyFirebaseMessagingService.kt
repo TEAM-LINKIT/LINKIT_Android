1120ec61c883b154a8d8916c0f6f8a784a680cf3
heewon <ymcho24@naver.com>
2021-05-30 16:42:27 +0900

    [fix] push 알림 icon 크기 조정 및 색상 변경

--- app/src/main/java/com/example/linkit_android/util/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/linkit_android/util/MyFirebaseMessagingService.kt
@@ -6,6 +6,7 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.media.RingtoneManager
 import android.os.Build
 import android.util.Log
@@ -63,13 +64,18 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         val soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
 
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
-            .setSmallIcon(R.mipmap.ic_launcher)
+            .setColor(Color.RED)
             .setContentTitle(title)
             .setContentText(body)
             .setAutoCancel(true)
             .setSound(soundUri)
             .setPriority(Notification.PRIORITY_HIGH)
             .setContentIntent(pendingIntent)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            notificationBuilder.setSmallIcon(R.drawable.ic_noti)
+        else
+            notificationBuilder.setSmallIcon(R.mipmap.ic_launcher)
 
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
